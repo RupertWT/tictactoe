ff6a35dd06ae7ce6541de2137266bb860358a9e8
IMPELLO\rwoolmertyler <IMPELLO\rwoolmertyler@hq-lap-2186.impello.co.uk>
2016-02-10 14:34:33 +0000

    Signed-off-by: IMPELLO\rwoolmertyler

--- src/main/java/com/rupert/tictactoe/TicTacToe.java
+++ src/main/java/com/rupert/tictactoe/TicTacToe.java
@@ -33,9 +33,53 @@ public class TicTacToe {
     	t3.init_components();
     	t3.init_icons();
     }
+   
     
     
+    // Initialise frame & buttons, listen for button clicks
+ 	public void init_components() {
+ 		JFrame frame = new JFrame();
+ 		frame.setDefaultCloseOperation(3);
+ 		frame.setVisible(true);
+ 		frame.setResizable(true);
+ 		frame.setSize(450,450);
+ 		JPanel panel = new JPanel();
+ 		frame.setContentPane(panel);
+ 		panel.setLayout(new GridLayout(3,3,0,0));
+ 				
+ 		panel.addComponentListener(new ComponentListener() {
+ 			public void componentResized(ComponentEvent e) {
+ 				
+ 				init_icons();
+ 			}
+ 			public void componentMoved(ComponentEvent e) {	
+ 			}
+ 			public void componentShown(ComponentEvent e) {	
+ 			}
+ 			public void componentHidden(ComponentEvent e) {
+ 			}		
+ 		});
+ 		
+ 		for (int i = 0; i < 9; i++){
+ 			final int pos = i;
+ 			
+ 			grid[i] = new JButton();
+ 			grid[i].addActionListener(new ActionListener() {
+ 				int id = pos;
+ 				
+ 				public void actionPerformed(ActionEvent e) {
+ 					play(id);
+ 				}
+ 				
+ 			});	
+ 			
+ 			panel.add(grid[i]);		
+ 		}	
+ 		frame.setSize(450,450);		
+ 	}
     
+ 	
+ 	
 	// Initialise Icons, images in res file, images scaled to button size
 	public void init_icons() {
 		
@@ -56,50 +100,7 @@ public class TicTacToe {
 		}
 	}
     
-    
-	
-	// Initialise frame & buttons, listen for button clicks
-	public void init_components() {
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(3);
-		frame.setVisible(true);
-		frame.setResizable(true);
-		frame.setSize(450,450);
-		JPanel panel = new JPanel();
-		frame.setContentPane(panel);
-		panel.setLayout(new GridLayout(3,3,0,0));
-		
-		panel.addComponentListener(new ComponentListener() {
-			public void componentResized(ComponentEvent e) {
-				init_icons();
-			}
-			public void componentMoved(ComponentEvent e) {	
-			}
-			public void componentShown(ComponentEvent e) {	
-			}
-			public void componentHidden(ComponentEvent e) {
-			}		
-		});
-		
-		for (int i = 0; i < 9; i++){
-			final int pos = i;
-			
-			grid[i] = new JButton();
-			grid[i].addActionListener(new ActionListener() {
-				int id = pos;
-				
-				public void actionPerformed(ActionEvent e) {
-					play(id);
-				}
-				
-			});	
-			
-			panel.add(grid[i]);		
-		}	
-		//frame.setSize(450,450);		
-	}
-	
-	
+    	
 	
 	// Play action, initiates attemptChange, checks for winning move then updates player
 	public void play(int id) {
