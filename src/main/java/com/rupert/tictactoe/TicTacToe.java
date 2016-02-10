3104568e5af274801e2eba3a7b2e3c4a661c6300
IMPELLO\rwoolmertyler <IMPELLO\rwoolmertyler@hq-lap-2186.impello.co.uk>
2016-02-10 14:14:05 +0000

    Signed-off-by: IMPELLO\rwoolmertyler

--- src/main/java/com/rupert/tictactoe/TicTacToe.java
+++ src/main/java/com/rupert/tictactoe/TicTacToe.java
@@ -18,15 +18,87 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class TicTacToe {
-	
-	
-	
+		
 	public JButton[] grid = new JButton[9];
 	public ImageIcon xIcon, oIcon;
 	public int[] board = new int[9];
 	public boolean won = false;
 	public int currentPlayer = 1;
 		
+	
+	
+	// Starts new game
+    public static void main( String[] args ) {
+    	TicTacToe t3 = new TicTacToe();
+    	t3.init_components();
+    	t3.init_icons();
+    }
+    
+    
+    
+	// Initialise Icons, images in res file, images scaled to button size
+	public void init_icons() {
+		
+		try{
+			Image x = ImageIO.read(TicTacToe.class.getResource("/x.png"));
+			Image o = ImageIO.read(TicTacToe.class.getResource("/o.png"));
+			
+			xIcon = new ImageIcon(x.getScaledInstance(grid[0].getWidth(), grid[0].getHeight(),Image.SCALE_SMOOTH));
+			oIcon = new ImageIcon(o.getScaledInstance(grid[0].getWidth(), grid[0].getHeight(),Image.SCALE_SMOOTH));
+			
+			for(int i=0; i<9; i++) {
+				if (grid[i].getIcon() != null) 
+						grid[i].setIcon(board[i] == 1 ? xIcon : oIcon);
+			}
+			
+		}catch(IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+    
+    
+	
+	// Initialise frame & buttons, listen for button clicks
+	public void init_components() {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(3);
+		frame.setVisible(true);
+		frame.setResizable(true);
+		//frame.setSize(450,450);
+		JPanel panel = new JPanel();
+		frame.setContentPane(panel);
+		panel.setLayout(new GridLayout(3,3,0,0));
+		
+		panel.addComponentListener(new ComponentListener() {
+			public void componentResized(ComponentEvent e) {
+				init_icons();
+			}
+			public void componentMoved(ComponentEvent e) {	
+			}
+			public void componentShown(ComponentEvent e) {	
+			}
+			public void componentHidden(ComponentEvent e) {
+			}		
+		});
+		
+		for (int i = 0; i < 9; i++){
+			final int pos = i;
+			
+			grid[i] = new JButton();
+			grid[i].addActionListener(new ActionListener() {
+				int id = pos;
+				
+				public void actionPerformed(ActionEvent e) {
+					play(id);
+				}
+				
+			});	
+			
+			panel.add(grid[i]);		
+		}	
+		frame.setSize(450,450);		
+	}
+	
 	
 	
 	// Play action, initiates attemptChange, checks for winning move then updates player
@@ -77,7 +149,7 @@ public class TicTacToe {
 	public void win(int square) {
 		won = true;
 		
-		String winner = board[square] == 1 ? "x" : "o";
+		String winner = board[square] == 1 ? "X" : "O";
 		
 		if(JOptionPane.showConfirmDialog(new JFrame("Winner! Congratulations " + winner), "Player " + winner + " has won! Would you like to play again?") == JOptionPane.YES_OPTION) {
 			reset();
@@ -110,77 +182,5 @@ public class TicTacToe {
 		}
 	}
 	
-	
-	
-	// Initialise frame & buttons, listen for button clicks
-	public void init_components() {
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(3);
-		frame.setVisible(true);
-		frame.setResizable(true);
-		//frame.setSize(450,450);
-		JPanel panel = new JPanel();
-		frame.setContentPane(panel);
-		panel.setLayout(new GridLayout(3,3,0,0));
-		
-		panel.addComponentListener(new ComponentListener() {
-			public void componentResized(ComponentEvent e) {
-				init_icons();
-			}
-			public void componentMoved(ComponentEvent e) {	
-			}
-			public void componentShown(ComponentEvent e) {	
-			}
-			public void componentHidden(ComponentEvent e) {
-			}		
-		});
-		
-		for (int i = 0; i < 9; i++){
-			final int pos = i;
-			
-			grid[i] = new JButton();
-			grid[i].addActionListener(new ActionListener() {
-				int id = pos;
-				
-				public void actionPerformed(ActionEvent e) {
-					play(id);
-				}
-				
-			});	
-			
-			panel.add(grid[i]);		
-		}	
-		frame.setSize(450,450);		
-	}
-	
-	
-	
-	// Initialise Icons, images in res file, images scaled to button size
-	public void init_icons() {
-		
-		try{
-			Image x = ImageIO.read(TicTacToe.class.getResource("/x.png"));
-			Image o = ImageIO.read(TicTacToe.class.getResource("/o.png"));
-			
-			xIcon = new ImageIcon(x.getScaledInstance(grid[0].getWidth(), grid[0].getHeight(),Image.SCALE_SMOOTH));
-			oIcon = new ImageIcon(o.getScaledInstance(grid[0].getWidth(), grid[0].getHeight(),Image.SCALE_SMOOTH));
-			
-			for(int i=0; i<9; i++) {
-				if (grid[i].getIcon() != null) 
-						grid[i].setIcon(board[i] == 1 ? xIcon : oIcon);
-			}
-			
-		}catch(IOException ex) {
-			ex.printStackTrace();
-		}
-	}
-	
-	
-	
-	// Starts new game
-    public static void main( String[] args ) {
-    	TicTacToe t3 = new TicTacToe();
-    	t3.init_components();
-    	t3.init_icons();
-    }
+
 }
